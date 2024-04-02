cecb2948b37c2aadab4043c34b3744a4a177cbec
latas-dev <thelonelysuric@gmail.com>
2024-04-01 23:00:21 -0600

    added Simon Game

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,84 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let gameStart = false;
+let level = 0; 
+
+$(document).keydown(function(event) {
+    if(event.key == 'a' && !gameStart) {
+        gameStart = true;
+        nextSequence();
+    }
+})
+
+let buttons = $(".btn");
+buttons.each(function(button) {
+    $(this).click(function() {
+        let userChosenColor = $(this).attr("id");
+        userClickedPattern.push(userChosenColor);
+        buttonSelect(userChosenColor);
+        if(userClickedPattern.length === gamePattern.length) {
+            checkAnswer(userClickedPattern.length - 1);
+        }
+    });
+});
+
+function endGame() {
+    gamePattern = [];
+    userClickedPattern = [];
+    gameStart = false;
+    level = 0;
+    let audio = new Audio("./sounds/wrong.mp3");
+    $("body").addClass("game-over");
+    audio.play();
+    setTimeout(function() {
+        $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("GAME OVER! Press Any Key to Restart");
+}
+
+function checkAnswer(currentLevel) {
+    for(let i = 0; i <= currentLevel; i++) {
+        if(userClickedPattern[i] === gamePattern[i]) {
+            console.log("success");
+        } else {
+            console.log("failure");
+            console.log("User: " + userClickedPattern);
+            console.log("Game: " + gamePattern);
+            endGame();
+            return;
+        }
+    }
+    console.log(userClickedPattern);
+    console.log(gamePattern);
+    setTimeout(function() {
+        nextSequence();
+    }, 1000);
+    userClickedPattern = [];
+}
+
+function changeLevel() {
+    $("h1").text("Level " + level);
+}
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4); 
+    let randomChosenColor = buttonColors[randomNumber];
+    level += 1;
+
+    gamePattern.push(randomChosenColor);
+
+    buttonSelect(randomChosenColor);
+    changeLevel();
+}
+
+function buttonSelect(color) {
+    let chosenButton = $("#" + color);
+    chosenButton.fadeOut(100).fadeIn(100);
+    playSound(color);
+}
+
+function playSound(color) {
+    let audio = new Audio("./sounds/" + color + ".mp3");
+    audio.play();
+}
--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,28 @@
 let heading = $('h1');
+let buttonList = $('.button-list');
+
+colorList = ['purple', 'yellow', 'cyan', 'pink', 'white', 'gray', 'brown'];
+for(let i = 0; i < colorList.length; i++) {
+    buttonList.append("<li><button class='" + colorList[i] + "'>Click for " + capitalize(colorList[i]) + "</button></li>");
+}
+
+let buttons = $('button');
+for(let i = 0; i < buttons.length; i++) {
+    $(buttons[i]).css('background-color', $(buttons[i]).attr('class'));
+    // $(buttons[i]).on('click', function() {
+    //     heading.css('color',$(this).attr('class'));
+    // });
+}
+buttons.on('click', function() {
+    heading.css('color', $(this).attr('class'));
+})
+
+$(document).keydown(function(event) {
+    if(event.key == 'Enter') buttons.slideToggle();
+
+    heading.text(event.key);
+})
+
+function capitalize(myString) {
+    return myString.charAt(0).toUpperCase() + myString.slice(1);
+}
